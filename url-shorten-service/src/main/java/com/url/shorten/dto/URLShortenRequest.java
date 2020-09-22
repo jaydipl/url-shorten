2c9a9cc69a3c0c4c70394d7de7acad677e75e5de
Jaydip lakhatariya <jaydip.lakhatariya@gmail.com>
2020-09-22 23:17:18 +0530

    Added validation and UML diagram

--- url-shorten-service/src/main/java/com/url/shorten/dto/URLShortenRequest.java
+++ url-shorten-service/src/main/java/com/url/shorten/dto/URLShortenRequest.java
@@ -1,11 +1,14 @@
 package com.url.shorten.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 /**
  * @author Jaydip
  *
  */
 public class URLShortenRequest {
-
+	
+	@NotEmpty(message = "Please provide longUrl")
 	private String longUrl;
 
 	public String getLongUrl() {
